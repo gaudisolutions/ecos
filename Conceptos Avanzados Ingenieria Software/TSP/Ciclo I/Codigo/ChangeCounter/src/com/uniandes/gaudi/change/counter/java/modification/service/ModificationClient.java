f439352fbd522b54d8d686d1d43c54502a98e1e0
Maria Paula Forero <ingsismapa89@hotmail.com>
2013-04-07 13:23:48 -0500

    no message

---
+++ Conceptos Avanzados Ingenieria Software/TSP/Ciclo I/Codigo/ChangeCounter/src/com/uniandes/gaudi/change/counter/java/modification/service/ModificationClient.java
@@ -0,0 +1,16 @@
+package com.uniandes.gaudi.change.counter.java.modification.service;
+
+import com.uniandes.gaudi.change.counter.entity.ChangeLOCStructure;
+import com.uniandes.gaudi.change.counter.modification.exception.ModificationServiceException;
+import com.uniandes.gaudi.change.counter.modification.service.ModificationService;
+
+public class ModificationClient implements ModificationService {
+
+	@Override
+	public void performLabelRegistry(ChangeLOCStructure changeLOCStructure)
+			throws ModificationServiceException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
